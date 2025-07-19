359c7c39b7fab0446b3b7f087c6b1ee6bf3a0dc7
joynirmal <joynirmal777@gmail.com>
2025-07-19 21:15:39 +0530

    Updated stored procedure(in parameter)

--- src/jdbc.java
+++ src/jdbc.java
@@ -1,9 +1,46 @@
 import java.sql.*;
 
 public class jdbc {
-    public static final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS student(id int NOT NULL PRIMARY KEY, name varchar(20), address varchar(20))";
-    public static final String INSERT_SQL = "INSERT INTO student(id, name, address) VALUES (?, ?, ?)";
-    public static final String SELECT_SQL = "SELECT * FROM student";
+    private static final String CREATE_SQL = "CREATE TABLE IF NOT EXISTS student(id int NOT NULL PRIMARY KEY, name varchar(20), address varchar(20))";
+    private static final String INSERT_SQL = "INSERT INTO student(id, name, address) VALUES (?, ?, ?)";
+    private static final String SELECT_SQL = "SELECT * FROM student";
+    private static final String SELECT_IN_PARA = "SELECT * FROM getId(?)";
+
+    public static void createTable(Connection con) throws SQLException {
+        try (PreparedStatement statement = con.prepareStatement(CREATE_SQL)) {
+            statement.execute();
+        }
+    }
+
+    public static void insertStudent(Connection con, int id, String name, String address) throws SQLException {
+        try (PreparedStatement statement = con.prepareStatement(INSERT_SQL)) {
+            statement.setInt(1, id);
+            statement.setString(2, name);
+            statement.setString(3, address);
+            int result = statement.executeUpdate();
+            System.out.println("\n" + result + " row/s affected");
+        }
+    }
+
+    public static void selectQuery(Connection con) throws SQLException{
+        try(PreparedStatement statement = con.prepareStatement(SELECT_SQL)){
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getString("address"));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void storedProcedure(Connection con,int id) throws SQLException{
+        try(PreparedStatement statement = con.prepareStatement(SELECT_IN_PARA)){
+            statement.setInt(1,id);
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getString("address"));
+            }
+        }
+    }
 
     public static void main(String[] args) throws Exception {
         String url = "jdbc:postgresql://localhost:5432/jdbcdemo";
@@ -14,35 +51,14 @@ public class jdbc {
             System.out.println("Connection Established");
             createTable(con);
             System.out.println("Table created");
-            insertStudent(con, 1, "John", "Madurai");
-            insertStudent(con,2,"Naveen","Tirunelveli");
-            selectQuery(con);
+            // insertStudent(con, 1, "John", "Madurai");
+            // insertStudent(con,2,"Naveen","Tirunelveli");
+            //insertStudent(con,3,"Ashwin","Tenkasi");
+            //selectQuery(con);
+            storedProcedure(con,2);
         }
     }
 
-    private static void createTable(Connection con) throws SQLException {
-        try (PreparedStatement statement = con.prepareStatement(CREATE_SQL)) {
-            statement.execute();
-        }
-    }
 
-    private static void insertStudent(Connection con, int id, String name, String address) throws SQLException {
-        try (PreparedStatement statement = con.prepareStatement(INSERT_SQL)) {
-            statement.setInt(1, id);
-            statement.setString(2, name);
-            statement.setString(3, address);
-            int result = statement.executeUpdate();
-            System.out.println(result + " row/s affected");
-        }
-    }
-
-    private static void selectQuery(Connection con) throws SQLException{
-        try(PreparedStatement statement = con.prepareStatement(SELECT_SQL)){
-            ResultSet rs = statement.executeQuery();
-            while (rs.next()){
-                System.out.println(rs.getInt("id") + " " + rs.getString("name") + " " + rs.getString("address"));
-            }
-        }
-    }
 }
 
